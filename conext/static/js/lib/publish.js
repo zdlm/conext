0e4b3df3be875d18fdbb2ba62ddc6b543d61aac0
leo <leo@leodeMacBook-Pro.local>
2014-06-29 12:03:52 +0800

    add content

--- conext/static/js/lib/publish.js
+++ conext/static/js/lib/publish.js
@@ -1,4 +1,4 @@
-define(['jquery','jquery.autosize'],function ($,autosize) {
+define(['jquery','jquery.autosize','text!../../html/publish.html'],function ($,autosize,html) {
     function publish(element) {
         this.element = $(element);
         this.render();
@@ -11,17 +11,44 @@ define(['jquery','jquery.autosize'],function ($,autosize) {
         },
 
         render: function () {
-            var self = this;
-            require(['text!../../html/publish.html'],function(html){
-                self.element.html(html);
-            });
+            this.element.html(html);
+//            var self = this;
+//            require(['text!../../html/publish.html'],function(html){
+//                self.element.html(html);
+//            });
+        },
+
+        _toTyping:function(){
+            $(".publish-content").addClass("publish-content-active");
+            $(".publish-operation").show();
+            $(".publish-close").show();
+        },
+
+        _close:function(){
+            $(".publish-content").removeClass("publish-content-active").val("").trigger('autosize.destroy');
+            $(".publish-operation").hide();
+            $(".publish-close").hide();
+            $(".publish-ok").attr("disabled",true);
+        },
+
+        _contentChange:function(){
+            var content = $(".publish-content");
+            var button = $(".publish-ok");
+            var value = content.val();
+            if(value.length > 0){
+                button.prop("disabled",false);
+            }else{
+                button.prop("disabled",true);
+            }
+            content.autosize();
+
         },
 
         _initEvents:function(){
-             $(".publish-content").bind("focus",function(){
-                 $(".publish-content").addClass("publish-content-active");
-                 $(".publish-operation").show();
-             });
+            $(".publish-content").bind("focus",this._toTyping);
+            $(".publish-close").bind("click",this._close);
+            $(".publish-content").bind("input",this._contentChange);
+            $(".publish-content").bind("onpropertychange",this._contentChange);
         }
     };
 
